9803d7868639b644de9c6a6bf0993b36ba624707
fatiiates <fatiiates@users.github.com>
2020-04-02 19:50:39 +0300

    Port
    Port değişikliği yapıldı.

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ router.get('/',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 80);
+app.listen(process.env.PORT);
 
-console.log('Running at Port 3000');
+console.log('Running');
